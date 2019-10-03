f778d514f84436feb7c0d4fa8007d3493a199a9c
unknown <juergen.riegler@gmx.net>
2019-10-03 20:05:12 +0200

    finished api

---
+++ ContactListAPI/ContactListAPI/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactListAPI
+{
+    public class Contact
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [MaxLength(70)]
+        public string Email { get; set; }
+
+        /*
+        public Contact(int id, string firstName, string lastName, string email)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public Contact() { }
+        */
+    }
+}
---
+++ ContactListAPI/ContactListAPI/Controllers/ContactsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ContactListAPI.Controllers
+{
+    [ApiController]
+    [Route("api/contacts")]
+    public class ContactsController: ControllerBase
+    {
+        public static readonly List<Contact> contacts = new List<Contact> {
+            new Contact { Id = 0, FirstName = "Eleasha", LastName = "Larsen", Email = "e.larsen@gmail.com"},
+            new Contact{ Id = 1, FirstName = "Braydon", LastName = "Mcmillan", Email = "braydon71@icloud.com"},
+            new Contact{ Id = 2, FirstName = "Eliot", LastName = "Page", Email = "contact@eliotpage.com" }
+        };
+
+        [HttpGet]
+        public IActionResult GetAllContacts() => Ok(contacts);
+
+        [HttpPost]
+        public IActionResult AddContact([FromBody] Contact newContact)
+        {
+            contacts.Add(newContact);
+            return CreatedAtRoute("GetSpecificContact", 
+                new { index = contacts.IndexOf(newContact) }, newContact);
+        }
+
+        [HttpDelete]
+        [Route("{personId}")]
+        public IActionResult DeleteContact(int personId)
+        {
+            if ( contacts.RemoveAll(c => c.Id == personId) > 0)
+                return NoContent();
+            return BadRequest("Invalid index");
+        }
+        
+        [HttpGet]
+        [Route("findByName")]
+        public IActionResult FindByName([FromBody] string nameFilter) 
+            => Ok(ContactsController.contacts.Where(c => c.FirstName.Contains(nameFilter) || c.LastName.Contains(nameFilter)));
+
+     }
+
+    }
